f2b07d11cc6af7f865cea80686a85f1d84919d6e
Kyle David <kyle.david@redline-electronics.com>
2012-09-06 21:50:41 -0400

    html writer working with basic google earth generation

--- traqpaqUSB/traqpaq_GUI/GoogleEarth.cs
+++ traqpaqUSB/traqpaq_GUI/GoogleEarth.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Web;
 using System.Web.UI;
+using System.Windows.Interop;
 
 namespace traqpaq_GUI
 {
@@ -19,12 +20,15 @@ namespace traqpaq_GUI
             InitializeComponent();
             //webBrowser.Url = new Uri(traqpaqResources.testGE);
             //webBrowser.DocumentText = traqpaqResources.testGE;
-            string result = loadGE();
-            // write to file
-            StreamWriter f = new StreamWriter(@"C:\Users\Kyle\traqpaq_usb_driver\traqpaqUSB\traqpaq_GUI\test\test.html");
-            f.Write(result);
-            f.Close();
-            f.Dispose();
+            //string result = loadGE();
+            //// write to file
+            //StreamWriter f = new StreamWriter(@"../..\test\test.html");
+            //f.Write(result);
+            //f.Close();
+            //f.Dispose();
+            //// Open in Notepad
+            //System.Diagnostics.Process.Start("notepad.exe", @"../..\test\test.html");
+
             webBrowser.DocumentText = loadGE();
         }
 
@@ -41,29 +45,38 @@ namespace traqpaq_GUI
             using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter))
             {
                 writer.RenderBeginTag(HtmlTextWriterTag.Html);
+
                 writer.RenderBeginTag(HtmlTextWriterTag.Head);
-                //writer.AddAttribute(HtmlTextWriterAttribute.Title, "Sample");
-                //writer.RenderBeginTag(HtmlTextWriterTag.Script);
+                writer.RenderBeginTag(HtmlTextWriterTag.Title);
+                writer.Write("Sample");
+                writer.RenderEndTag();  // End title tag
+                
                 writer.AddAttribute(HtmlTextWriterAttribute.Type, "text/javascript");
                 writer.AddAttribute(HtmlTextWriterAttribute.Src, "https://www.google.com/jsapi");
-                //writer.RenderEndTag(); // closes the <script> tag
-
-                // main GE code. start new script tag
                 writer.RenderBeginTag(HtmlTextWriterTag.Script);
+                writer.RenderEndTag();  // closes first script tag
+                
+                // main GE code. start new script tag
+                writer.AddAttribute("type", "text/javascript");
+                writer.RenderBeginTag(HtmlTextWriterTag.Script);
+                writer.WriteLine("var ge");
                 writer.WriteLine("google.load(\"earth\", \"1\");");
+
+                writer.WriteLine("function init() {\n\tgoogle.earth.createInstance('map3d', initCB, failureCB);\n}\n");
+
                 writer.WriteLine("function initCB(instance) {");
-                writer.WriteLine("ge = instance;");
-                writer.WriteLine("ge.getWindow().setVisibility(true);\n}");
-                writer.WriteLine("\nfunction failureCB(errorCode) {\n}\n");
+                writer.WriteLine("\tge = instance;");
+                writer.WriteLine("\tge.getWindow().setVisibility(true);\n}\n");
+                writer.WriteLine("function failureCB(errorCode) {\n}\n");
                 writer.WriteLine("google.setOnLoadCallback(init);");
                 writer.RenderEndTag();  // closes script tag
 
                 writer.RenderEndTag();  // closes head tag
 
-                writer.RenderBeginTag(HtmlTextWriterTag.Body);
-                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                writer.RenderBeginTag(HtmlTextWriterTag.Body);                
                 writer.AddAttribute(HtmlTextWriterAttribute.Id, "map3d");
                 writer.AddAttribute(HtmlTextWriterAttribute.Style, "height: 400px; width: 600px;");
+                writer.RenderBeginTag(HtmlTextWriterTag.Div);
                 writer.RenderEndTag();  // close div tag
                 writer.RenderEndTag();  // close body tag
                 writer.RenderEndTag();  // closes html tag
