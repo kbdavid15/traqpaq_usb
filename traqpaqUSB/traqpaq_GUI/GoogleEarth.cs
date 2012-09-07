bebd8dc98c3c6ad15995b20e3fe2adc5c0e2ae36
Kyle David <kyle.david@redline-electronics.com>
2012-09-06 22:05:05 -0400

    added some more ge functions

--- traqpaqUSB/traqpaq_GUI/GoogleEarth.cs
+++ traqpaqUSB/traqpaq_GUI/GoogleEarth.cs
@@ -20,14 +20,14 @@ namespace traqpaq_GUI
             InitializeComponent();
             //webBrowser.Url = new Uri(traqpaqResources.testGE);
             //webBrowser.DocumentText = traqpaqResources.testGE;
-            //string result = loadGE();
-            //// write to file
-            //StreamWriter f = new StreamWriter(@"../..\test\test.html");
-            //f.Write(result);
-            //f.Close();
-            //f.Dispose();
-            //// Open in Notepad
-            //System.Diagnostics.Process.Start("notepad.exe", @"../..\test\test.html");
+            string result = loadGE();
+            // write to file
+            StreamWriter f = new StreamWriter(@"../..\test\test.html");
+            f.Write(result);
+            f.Close();
+            f.Dispose();
+            // Open in Notepad
+            System.Diagnostics.Process.Start("notepad.exe", @"../..\test\test.html");
 
             webBrowser.DocumentText = loadGE();
         }
@@ -62,11 +62,22 @@ namespace traqpaq_GUI
                 writer.WriteLine("var ge");
                 writer.WriteLine("google.load(\"earth\", \"1\");");
 
-                writer.WriteLine("function init() {\n\tgoogle.earth.createInstance('map3d', initCB, failureCB);\n}\n");
+                writer.WriteLine("function init() {");
+                writer.WriteLine("\tgoogle.earth.createInstance('map3d', initCB, failureCB);");
+                writer.WriteLine("}");
 
+                // initialize callback function
                 writer.WriteLine("function initCB(instance) {");
                 writer.WriteLine("\tge = instance;");
-                writer.WriteLine("\tge.getWindow().setVisibility(true);\n}\n");
+                writer.WriteLine("\tge.getWindow().setVisibility(true);");
+                // add a navigation control
+                writer.WriteLine("ge.getNavigationControl().setVisibility(ge.VISIBILITY_AUTO);");
+                // add some layers
+                writer.WriteLine("ge.getLayerRoot().enableLayerById(ge.LAYER_BORDERS, true);");
+                writer.WriteLine("ge.getLayerRoot().enableLayerById(ge.LAYER_ROADS, true);");
+                writer.WriteLine("}");
+
+                // failure callback function
                 writer.WriteLine("function failureCB(errorCode) {\n}\n");
                 writer.WriteLine("google.setOnLoadCallback(init);");
                 writer.RenderEndTag();  // closes script tag
