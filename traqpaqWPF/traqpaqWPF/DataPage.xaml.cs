de84ad498682440d6e7b2244eba7553dee4f5f16
Kyle David <kyle.david@redline-electronics.com>
2012-09-15 21:07:13 -0400

    more test coordinates, modified html

--- traqpaqWPF/traqpaqWPF/DataPage.xaml.cs
+++ traqpaqWPF/traqpaqWPF/DataPage.xaml.cs
@@ -22,8 +22,9 @@ namespace traqpaqWPF
         public string LapTime { get; set; }
         public Color LapColor { get; set; }
         public int LineIndex { get; set; }
-       // public bool IsSelected { get; set; }
         public bool IsRendered { get; set; }
+        public List<double> latitudes;
+        public List<double> longitudes;
     }
 
     /// <summary>
@@ -31,6 +32,19 @@ namespace traqpaqWPF
     /// </summary>
     public partial class DataPage : Page
     {
+        List<double> latitudes1 = new List<double> { 42.1838786, 42.17332503, 42.1607474, 42.15192871, 
+                                                            42.14788076, 42.1538081, 42.16493994, 42.17462615,
+                                                            42.18286665, 42.1838786, 42.1838786};
+        List<double> longitudes1 = new List<double> { -83.32956822, -83.34756995, -83.35109965, -83.34209883,
+                                                             -83.32321473, -83.31368443, -83.30027137, -83.30556597,
+                                                             -83.3188025, -83.32939178, -83.32939178 };
+        List<double> latitudes2 = new List<double> { 42.1848786, 42.17432503, 42.1617474, 42.15292871, 
+                                                            42.14888076, 42.1548081, 42.16593994, 42.17562615,
+                                                            42.18386665, 42.1848786, 42.1848786};
+        List<double> longitudes2 = new List<double> { -83.32956822, -83.34756995, -83.35109965, -83.34209883,
+                                                             -83.32321473, -83.31368443, -83.30027137, -83.30556597,
+                                                             -83.3188025, -83.32939178, -83.32939178 };
+        
         MainWindow main;
         GoogleEarthWebBrowser geBrowser;
 
@@ -56,8 +70,8 @@ namespace traqpaqWPF
             mainGrid.Children.Add(geBrowser);
             Grid.SetColumn(geBrowser, 1);
 
-            _LapCollection.Add(new LapInfo { LapNo = "1", LapTime = "2:30", LapColor = Colors.LawnGreen });
-            _LapCollection.Add(new LapInfo { LapNo = "2", LapTime = "2:24", LapColor = Colors.LightSlateGray });
+            _LapCollection.Add(new LapInfo { LapNo = "1", LapTime = "2:30", LapColor = Colors.LawnGreen, latitudes = latitudes1, longitudes = longitudes1 });
+            _LapCollection.Add(new LapInfo { LapNo = "2", LapTime = "2:24", LapColor = Colors.LightSlateGray, latitudes = latitudes2, longitudes = longitudes2 });
 
         }
 
@@ -73,18 +87,11 @@ namespace traqpaqWPF
             {
                 // use this to determine average lap time, average speed, max speed, etc
                 // show a message box with the selected color
-                List<double> latitudes = new List<double> { 42.1838786, 42.17332503, 42.1607474, 42.15192871, 
-                                                            42.14788076, 42.1538081, 42.16493994, 42.17462615,
-                                                            42.18286665, 42.1838786, 42.1838786};
-                List<double> longitudes = new List<double> { -83.32956822, -83.34756995, -83.35109965, -83.34209883,
-                                                             -83.32321473, -83.31368443, -83.30027137, -83.30556597,
-                                                             -83.3188025, -83.32939178, -83.32939178 };
+                
                 //string kml = KmlCreator.getKMLstring(item.LapColor, latitudes, longitudes);
                 //geBrowser.loadKML(kml);
-                //MessageBox.Show(item.IsSelected.ToString());
-                geBrowser.addPoints(latitudes, longitudes, "#" + item.LapColor.ToString().Substring(3), item.LapNo);
+                geBrowser.addPoints(item.latitudes, item.longitudes, "#" + item.LapColor.ToString().Substring(3), item.LapNo);
                 item.IsRendered = true;
-                //TODO test removal of lap
             }
         }
 
@@ -100,6 +107,7 @@ namespace traqpaqWPF
                 if (!listViewLaps.SelectedItems.Contains(item) && item.IsRendered)
                 {
                     geBrowser.removeLap(item.LapNo);
+                    item.IsRendered = false;
                 }
             }
         }
